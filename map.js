a0d4db956d7bf148eeaea25e37dd463bf30846f3
RyanLB96 <ryanlb96@gmail.com>
2018-02-27 19:39:54 +0000

    update

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
       function initMap() {
         var uluru = {lat: 52.948574, lng: -1.169075};
-        var uluru2 = {lat: 53.948574, lng: -1.169075};
+        var uluru2 = {lat: 52.948674, lng: -1.169075};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
           center: uluru
@@ -13,12 +13,7 @@
           position: uluru2,
           map: map
         });
-        var myLatlng2 = new google.maps.LatLng(45.9,-85.6);
 
-var marker3 = new google.maps.Marker({
-    position: myLatlng2,
-    map: map
-});
       }
 
 
