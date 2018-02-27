ea72d962f35dbc26af1195dd72c8b9c5bd5f4787
RyanLB96 <ryanlb96@gmail.com>
2018-02-27 21:55:33 +0000

    update

--- map.js
+++ map.js
@@ -1,8 +1,8 @@
 function initMap() {
         var onefivethree = {lat: 52.948574, lng: -1.169075};
         var onefiveone = {lat: 52.948674, lng: -1.169075};
-        //var Abs1 =   {lat: 52.9513289,  lng: -1.1750136000000566};
-        //var Abs2 =   {lat: 52.9558919,  lng: -1.153685600000017};
+        var Abs1 =   {lat: 52.9513289,  lng: -1.1750136000000566};
+        var Abs2 =   {lat: 52.9558919,  lng: -1.153685600000017};
 
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
@@ -14,6 +14,14 @@ function initMap() {
         });
         var marker2 = new google.maps.Marker({
           position: onefiveone,
+          map: map
+        });
+        var marker3 = new google.maps.Marker({
+          position: Abs1,
+          map: map
+        });
+        var marker4 = new google.maps.Marker({
+          position: Abs2,
           map: map
         });
       }
