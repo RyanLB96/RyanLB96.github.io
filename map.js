66e77db6cf77c8f7e2565e50056c8685f859c4cd
RyanLB96 <ryanlb96@gmail.com>
2018-02-27 21:53:52 +0000

    update

--- map.js
+++ map.js
@@ -1,20 +1,35 @@
-      function initMap() {
-        var uluru = {lat: 52.948574, lng: -1.169075};
-        var uluru2 = {lat: 52.948674, lng: -1.169075};
+function initMap() {
+        var onefivethree = {lat: 52.948574, lng: -1.169075};
+        var onefiveone = {lat: 52.948674, lng: -1.169075};
+        //var Abs1 =   {lat: 52.9513289,  lng: -1.1750136000000566};
+        //var Abs2 =   {lat: 52.9558919,  lng: -1.153685600000017};
+
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
-          center: uluru
+          center: onefivethree
         });
         var marker = new google.maps.Marker({
-          position: uluru,
+          position: onefivethree,
           map: map
         });
         var marker2 = new google.maps.Marker({
-          position: uluru2,
+          position: onefiveone,
           map: map
         });
-
       }
 
+//var image = 'Capture4.jpg'
 
+//var AbsMarker = new google.maps.Marker({
+//  position: Abs1,
+//  map: map,
+//  icon: image
+//});
+//var image2 = 'Capture3.jpg'
+//var AbsMarker2 = new google.maps.Marker({
+//  position: Abs2,
+//  map: map,
+//  icon: image2
+//});
+      }
 
