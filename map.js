4d0451391bff7af2bbc86e0c70849353dfcb6827
RyanLB96 <ryanlb96@gmail.com>
2018-02-28 00:17:23 +0000

    update

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
 function initMap() {
+//Markers variables
         var onefivethree = {lat: 52.948574, lng: -1.169075};
         var onefiveone = {lat: 52.948674, lng: -1.169075};
-        var Abs1 =   {lat: 52.9513289,  lng: -1.1750136000000566};
+        var Abs1 =   {lat: 52.95155514119412,  lng: -1.1754856687866777};
         var Abs2 =   {lat: 52.9558919,  lng: -1.153685600000017};
 
         var Absimage1 = {
@@ -9,14 +10,18 @@ function initMap() {
             scaledSize: new google.maps.Size(100, 200)};
         var Absimage2 = {
             url: 'Capture3.JPG',
-            scaledSize: new google.maps.Size(100, 100)};         
+            scaledSize: new google.maps.Size(100, 100)};  
+//Line variables
+        var lineSymbol = {
+          path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
+        };       
 
-
+//Map
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
           center: onefivethree
         });
-
+//4 Markers
         var marker = new google.maps.Marker({
           position: onefivethree,
           map: map
@@ -37,6 +42,14 @@ function initMap() {
           map: map,
           icon: Absimage2
         });
-      
+      //Line
+      var line = new google.maps.Polyline({
+          path: [{lat: 52.95155514119412, lng: -1.1754856687866777}, {lat: 52.9513289, lng: -1.1750136000000566}],
+          icons: [{
+            icon: lineSymbol,
+            offset: '100%'
+          }],
+          map: map
+        });
       }
 
