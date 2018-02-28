bcd7756516fffe00726cefa16808691e2d27ed43
RyanLB96 <ryanlb96@gmail.com>
2018-02-28 00:59:52 +0000

    update

--- map.js
+++ map.js
@@ -86,7 +86,7 @@ function definePopupClass() {
    * @constructor
    * @extends {google.maps.OverlayView}
    */
-  Popup = function(position, content) {
+  Popup = function(position, contentpopup1) {
     this.position = position;
 
     contentpopup.classList.add('popup-bubble-content');
